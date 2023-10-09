3266d29678a69d2c2f374ebd064738bf3f564f56
PARTH SUHAS SHIRSATH <129524737+ParthShirsath@users.noreply.github.com>
2023-10-09 16:53:30 +0530

    Add files via upload

---
+++ nth_element_d_linklist.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<ctype.h>
+#define MEMORY (struct node *)malloc(sizeof(struct node))
+typedef struct node{
+    int data;
+    struct node *next,*prev;
+}NODE;
+NODE *create(NODE *head)
+{
+    NODE *newnode,*temp;
+    int n,i;
+     printf("Enter limit=");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        newnode=MEMORY;
+        newnode->next=newnode->prev=NULL;
+        printf("enter number");
+        scanf("%d",&newnode->data);
+        if(head==NULL)
+        {
+            temp=head=newnode;
+        }
+        else
+        {
+            temp->next=newnode;
+            newnode->prev=newnode;
+            temp=newnode;
+        }
+    }
+    return head;
+}
+void disp(NODE *head)
+{
+    NODE *temp;
+    for(temp=head;temp!=NULL;temp=temp->next)
+    {
+        printf("%d    ",temp->data);
+    }
+}
+void npos(NODE *head)
+{
+    NODE *temp;
+    for(temp=head;temp->next!=NULL;temp=temp->next);
+    printf("%d\t",temp->data);
+}
+int main()
+{
+    NODE *head=NULL;
+    head=create(head);
+    disp(head);
+    printf("\n\n");
+    npos(head);
+}
